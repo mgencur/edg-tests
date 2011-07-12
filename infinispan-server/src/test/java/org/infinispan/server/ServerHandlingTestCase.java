979fffee1eef4b8d86d8fe7882489abdeda0d376
mgencur@redhat.com <mgencur@redhat.com>
2011-07-12 16:12:15 +0200

    simple test for ispn-arquillian

---
+++ infinispan-server/src/test/java/org/infinispan/server/ServerHandlingTestCase.java
@@ -0,0 +1,44 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2011 Red Hat Inc. and/or its affiliates and other contributors
+ * as indicated by the @authors tag. All rights reserved.
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,  
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.infinispan.server;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.TargetsContainer;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(Arquillian.class)
+public class ServerHandlingTestCase {
+   
+    private static final String CONTAINER1 = "container1"; 
+    private static final String DEPLOYMENT1 = "dep.container1"; 
+   
+    @Deployment(name = DEPLOYMENT1, managed=true)
+    @TargetsContainer(CONTAINER1)
+    public static WebArchive createTestDeployment() {
+        return Deployments.createActiveClient();
+    }
+    
+    @Test
+    public void startStopTest() throws Exception {
+       System.out.println("=== Inside startStopTest ===");
+    }
+    
+}
