758c9c09484a0f0f5950dd0730679453ffeb88eb
mgencur@redhat.com <mgencur@redhat.com>
2011-07-19 16:54:02 +0200

    test retrieving Infinispan server statistics

--- infinispan-server/src/test/java/org/infinispan/server/ServerHandlingTestCase.java
+++ infinispan-server/src/test/java/org/infinispan/server/ServerHandlingTestCase.java
@@ -18,16 +18,86 @@
 package org.infinispan.server;
 
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.infinispan.arquillian.core.Infinispan;
+import org.jboss.infinispan.arquillian.core.InfinispanInfo;
+import org.jboss.infinispan.arquillian.model.CacheStatistics;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(Arquillian.class)
 public class ServerHandlingTestCase {
    
-    //by default runs in Client mode (when no @Deployment is present)
+    @Infinispan("container1")
+    InfinispanInfo info1;
+    
+    @Infinispan("container2")
+    InfinispanInfo info2;
+   
     @Test
-    public void startStopTest() throws Exception {
-       System.out.println("=== Inside startStopTest ===");
+    public void shouldRetrieveGeneralInformation() throws Exception 
+    {
+       System.out.println("=== General info ===");
+       System.out.println("port: " + info2.getMemcachedEndpoint().getPort());
+       System.out.println("hostname: " + info2.getMemcachedEndpoint().getInetAddress());
+    }
+    
+    @Test
+    public void shouldRetrieveCacheManagerInformation()
+    {
+       System.out.println("=== CacheManager info ===");
+       System.out.println(info1.getCacheManager("DefaultCacheManager").getCreatedCacheCount());
+       System.out.println(info1.getDefaultCacheManager().getCacheManagerStatus());
+       System.out.println(info1.getDefaultCacheManager().getCacheName());
+       System.out.println(info1.getDefaultCacheManager().getClusterMembers());
+       System.out.println(info1.getDefaultCacheManager().getClusterSize());
+       System.out.println(info1.getDefaultCacheManager().getCreatedCacheCount());
+       System.out.println(info1.getDefaultCacheManager().getDefinedCacheCount());
+       System.out.println(info1.getDefaultCacheManager().getDefinedCacheNames());
+       System.out.println(info1.getDefaultCacheManager().getNodeAddress());
+       System.out.println(info1.getDefaultCacheManager().getPhysicalAddresses());
+       System.out.println(info1.getDefaultCacheManager().getRunningCacheCount());
+       System.out.println(info1.getDefaultCacheManager().getVersion());
+    }
+    
+    @Test
+    public void shouldRetrieveCacheInformation()
+    {
+       System.out.println("=== Cache info ===");
+       System.out.println(info1.getDefaultCacheManager().getDefaultCache().getCacheName());
+       System.out.println(info1.getDefaultCacheManager().getDefaultCache().getCacheStatus());
+       System.out.println(info1.getDefaultCacheManager().getDefaultCache().getAverageReadTime());
+       System.out.println(info1.getDefaultCacheManager().getDefaultCache().getAverageWriteTime());
+       System.out.println(info1.getDefaultCacheManager().getDefaultCache().getElapsedTime());
+       System.out.println(info1.getDefaultCacheManager().getDefaultCache().getEvictions());
+       System.out.println(info1.getDefaultCacheManager().getDefaultCache().getHitRatio());
+       System.out.println(info1.getDefaultCacheManager().getDefaultCache().getHits());
+       System.out.println(info1.getDefaultCacheManager().getDefaultCache().getMisses());
+       System.out.println(info1.getDefaultCacheManager().getDefaultCache().getNumberOfEntries());
+       System.out.println(info1.getDefaultCacheManager().getDefaultCache().getReadWriteRatio());
+       System.out.println(info1.getDefaultCacheManager().getDefaultCache().getRemoveHits());
+       System.out.println(info1.getDefaultCacheManager().getDefaultCache().getRemoveMisses());
+       System.out.println(info1.getDefaultCacheManager().getDefaultCache().getStores());
+       System.out.println(info1.getDefaultCacheManager().getDefaultCache().getTimeSinceReset());
+       
+       //getting statistics via a common method getStatistics
+       System.out.println(info1.getDefaultCacheManager().getDefaultCache().getStatistics(CacheStatistics.AVERAGE_READ_TIME));
+       System.out.println(info1.getDefaultCacheManager().getDefaultCache().getStatistics(CacheStatistics.AVERAGE_WRITE_TIME));
+       System.out.println(info1.getDefaultCacheManager().getDefaultCache().getStatistics(CacheStatistics.ELAPSED_TIME));
+       System.out.println(info1.getDefaultCacheManager().getDefaultCache().getStatistics(CacheStatistics.EVICTIONS));
+       System.out.println(info1.getDefaultCacheManager().getDefaultCache().getStatistics(CacheStatistics.HIT_RATIO));
+       System.out.println(info1.getDefaultCacheManager().getDefaultCache().getStatistics(CacheStatistics.HITS));
+       System.out.println(info1.getDefaultCacheManager().getDefaultCache().getStatistics(CacheStatistics.MISSES));
+       System.out.println(info1.getDefaultCacheManager().getDefaultCache().getStatistics(CacheStatistics.NUMBER_OF_ENTRIES));
+       System.out.println(info1.getDefaultCacheManager().getDefaultCache().getStatistics(CacheStatistics.READ_WRITE_RATIO));
+       System.out.println(info1.getDefaultCacheManager().getDefaultCache().getStatistics(CacheStatistics.REMOVE_HITS));
+       System.out.println(info1.getDefaultCacheManager().getDefaultCache().getStatistics(CacheStatistics.REMOVE_MISSES));
+       System.out.println(info1.getDefaultCacheManager().getDefaultCache().getStatistics(CacheStatistics.STORES));
+       System.out.println(info1.getDefaultCacheManager().getDefaultCache().getStatistics(CacheStatistics.TIME_SINCE_RESET));
+    }
+    
+    @Test
+    public void infinispanInfoTest(@Infinispan("container2") InfinispanInfo info3) {
+       //also possible to inject into method parameters
     }
     
 }
